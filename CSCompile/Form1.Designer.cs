3cf1da7527e5d6c35aea26e1d214763f9fbabebe
encodeous <47889571+encodeous@users.noreply.github.com>
2019-12-29 19:07:51 -0500

    Upload of Archived Code

---
+++ CSCompile/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace CSCompile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.AcceptsTab = true;
+            this.TextBox1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.TextBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox1.ForeColor = System.Drawing.Color.White;
+            this.TextBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.TextBox1.Location = new System.Drawing.Point(3, 23);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.ShowSelectionMargin = true;
+            this.TextBox1.Size = new System.Drawing.Size(1327, 505);
+            this.TextBox1.TabIndex = 0;
+            this.TextBox1.Text = "";
+            this.TextBox1.WordWrap = false;
+            this.TextBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(0, 711);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(680, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Build And Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(2, 546);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1327, 162);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 531);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Output";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(677, 711);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(651, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Just run";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1227, 530);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Not Compiled Yet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(455, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "CSCompile - A portable C# compiler and editor by: Encodeous (Note Only Console Ap" +
+    "plications)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(530, -2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 25);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Console Application";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(476, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Presets";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1328, 746);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "CSCompile";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CSCompile/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace CSCompile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool textchange = true;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textchange)
+            {
+                MessageBox.Show("Please Write Some C0de in the box.","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else
+            {
+                richTextBox1.Text = "";
+                CodeDomProvider codeProvider = CodeDomProvider.CreateProvider("CSharp");
+                string Output = "Out.exe";
+                Button ButtonObject = (Button)sender;
+
+                System.CodeDom.Compiler.CompilerParameters parameters = new CompilerParameters();
+                //Make sure we generate an EXE, not a DLL
+                parameters.GenerateExecutable = true;
+                parameters.OutputAssembly = Output;
+                CompilerResults results = codeProvider.CompileAssemblyFromSource(parameters, TextBox1.Text);
+
+                if (results.Errors.Count > 0)
+                {
+                    label2.ForeColor = Color.Red;
+                    label2.Text = "Built Failed!";
+                    richTextBox1.ForeColor = Color.Red;
+                    foreach (CompilerError CompErr in results.Errors)
+                    {
+                        richTextBox1.Text = richTextBox1.Text +
+                                    "Line number " + CompErr.Line +
+                                    ", Error Number: " + CompErr.ErrorNumber +
+                                    ", '" + CompErr.ErrorText + ";" +
+                                    Environment.NewLine + Environment.NewLine;
+                    }
+                }
+                else
+                {
+                    //Successful Compile
+                    label2.ForeColor = Color.Blue;
+                    label2.Text = "Built Successfully!";
+                    //If we clicked run then launch our EXE
+
+                    string folderPath = string.Empty;
+                    // folderBrowserDialog1.ShowDialog();
+                    if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        folderPath = folderBrowserDialog1.SelectedPath;
+                        File.Copy("Out.exe", folderPath + @"\CompiledAssembly.exe");
+                        File.Delete("Out.exe");
+                    }
+                }
+            }
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textchange)
+            {
+                MessageBox.Show("Please Write Some C0de in the box.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                richTextBox1.Text = "";
+                CodeDomProvider codeProvider = CodeDomProvider.CreateProvider("CSharp");
+                string Outputs = "Out.exe";
+                Button ButtonObject = (Button)sender;
+
+                System.CodeDom.Compiler.CompilerParameters parameters = new CompilerParameters();
+                //Make sure we generate an EXE, not a DLL
+                parameters.GenerateExecutable = true;
+                parameters.OutputAssembly = Outputs;
+                CompilerResults results = codeProvider.CompileAssemblyFromSource(parameters, TextBox1.Text);
+
+                if (results.Errors.Count > 0)
+                {
+                    label2.ForeColor = Color.Red;
+                    label2.Text = "Built Failed!";
+                    richTextBox1.ForeColor = Color.Red;
+                    foreach (CompilerError CompErr in results.Errors)
+                    {
+                        richTextBox1.Text = richTextBox1.Text +
+                                    "Line number " + CompErr.Line +
+                                    ", Error Number: " + CompErr.ErrorNumber +
+                                    ", '" + CompErr.ErrorText + ";" +
+                                    Environment.NewLine + Environment.NewLine;
+                    }
+                }
+                else
+                {
+                    //Successful Compile
+                    label2.ForeColor = Color.Blue;
+                    label2.Text = "Built Successfully!";
+                    //If we clicked run then launch our EXE
+                    Process process = new Process();
+                    // Configure the process using the StartInfo properties.
+                    process.StartInfo.FileName = "Out.exe";
+                    process.Start();
+                    process.WaitForExit();
+                    File.Delete("Out.exe");
+                }
+            }
+            
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            textchange = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = @"
+using System;
+
+public class Compile
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(DateTime.Today.ToString());
+        Console.ReadLine();
+    }
+}";
+            textchange = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = @"
+
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace CSCompile
+{
+    public partial class FMDemo : Form
+    {
+        public FMDemo()
+        {
+            InitializeComponent();
+        }
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 23);
+            this.button1.Name = ""button1"";
+            this.button1.Size = new System.Drawing.Size(188, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = ""<<THE Useless Button>>"";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FMDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 115);
+            this.Controls.Add(this.button1);
+            this.Name = ""FMDemo"";
+            this.Text = ""Demo Form"";
+            this.ResumeLayout(false);
+
+        }
+        private void FMDemo_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+";
+            textchange = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
